0eeb4f960fb4de37a938af87542a47a5f36cc753
Jose Copeti <jrcopeti@gmail.com>
2023-02-04 17:36:01 +0100

    models validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,16 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belong_to :art
-  validates :price, :rent_type, :start_date, :end_date, :shipping_address, presence: true
+  validates :start_date, :end_date, presence: true, availability: true
+  validates :end_date_after_start_date
+  validates :price, :rent_type, :shipping_address, presence: true
   enum :rent_type, { private_use: 0, events: 1, museums: 2, exhibition: 3, profitable: 4 }
+
+  private
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    errors.add(:end_date, "must be after the start date") if end_date < start_date
+  end
 end
