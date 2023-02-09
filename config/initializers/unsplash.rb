a4ab9c60f25f646ab882e608550c9e61b480565d
Jose Copeti <jrcopeti@gmail.com>
2023-02-09 16:08:58 +0100

    db seeded

--- config/initializers/unsplash.rb
+++
@@ -1,6 +0,0 @@
-Unsplash.configure do |config|
-  config.application_access_key = "Xs8FD-nKCfKM3PgeJ2ZbUHz4zVfmQDOaE9lxIF7caLk"
-  config.application_secret = "x4jprACNG2gaR7b2gOpKwnyIDbqhzpBLOOHMG9fL8o8"
-  config.utm_source = 'arttraktive'
-end
-
