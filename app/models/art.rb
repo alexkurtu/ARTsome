09676352d6090d09906f3d92923c4bd0a64853c9
Jose Copeti <jrcopeti@gmail.com>
2023-02-11 14:03:38 +0100

    latest backend

--- app/models/art.rb
+++ app/models/art.rb
@@ -3,6 +3,6 @@ class Art < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
 
-  validates :category, :description, :title, :picture, :artist, :year, :current_location, :price, presence: true
+  validates :category, :description, :title, :artist, :year, :current_location, :price, presence: true
   has_many_attached :photos
 end
