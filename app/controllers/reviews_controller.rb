655f3e74b54c705869e4f8898387c7eae1831a15
Alex Kurtu <alex-kurtu@hotmail.com>
2023-02-16 21:39:26 +0100

    Create Review Fields

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :set_booking, only: [:new, :create]
+  # before_action :set_booking, only: [:new, :create]
 
   def new
     @review = Review.new
@@ -7,16 +7,20 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
-    @review.booking = @booking
-    @review.save
-    redirect_to bookings_path
+    @art = Art.find(params[:art_id])
+    @user = current_user
+    @review.user = @user
+    @review.art = @art
+    raise
+    @review.save!
+    redirect_to art_path(@art)
   end
 
   private
 
-  def set_booking
-    @booking = Booking.find(params[:booking_id])
-  end
+  # def set_booking
+  #   @booking = Booking.find(params[:booking_id])
+  # end
 
   def review_params
     params.require(:review).permit(:content, :rating)
